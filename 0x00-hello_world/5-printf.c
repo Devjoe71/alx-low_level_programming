f01a02549590efa296225a30cafb44d6fe00417e
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-05 09:58:49 -0700

    task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints out the outcome which is a piece of art
+ *
+ * Return: zero for this case
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
