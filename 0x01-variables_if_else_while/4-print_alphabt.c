99aa2544a9cd613ed46c389152b838955c28fb54
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-07 06:19:32 -0700

    task 4 without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,12 @@
  */
 int main(void)
 {
-	char i = 'a';
+	char i;
 
-	while (i <= 'z')
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i != 'q' && i != 'e')
-		{
 			putchar(i);
-			i++;
-		}
 	}
 	putchar('\n');
 	return (0);
