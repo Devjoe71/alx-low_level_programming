53e57eedbb14166d3023b4befafcc0a260c557b8
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-07 05:05:22 -0700

    task 3 alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - prints bothalphabets in  lowercase and uppercase
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	char i = 'a';
+	char j = 'A';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	while (j <= 'Z')
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
+
