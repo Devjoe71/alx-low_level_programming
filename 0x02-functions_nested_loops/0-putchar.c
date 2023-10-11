b0498c0804762b4d4f30a4e3843be666bc0307a2
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-11 09:36:47 -0700

    task 0_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "my_main.h"
+
+/**
+ * main - prints out _putchar
+ *
+ * Return: it returns zero
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
+	return (0);
+}
