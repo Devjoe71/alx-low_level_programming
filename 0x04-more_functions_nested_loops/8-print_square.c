2656df8964a6dabecfdb6a7eda71a726a11046cf
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-13 13:23:18 -0700

    task 8 prints square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - prints a square followed by square
+ *
+ * @size: checks for parameters
+ * Return: zero
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int n, m;
+
+		for (n = 0; n <= size; n++)
+		{
+			for (m = 0; m <= size; m++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
+
