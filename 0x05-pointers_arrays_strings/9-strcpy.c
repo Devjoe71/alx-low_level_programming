646661b0abfbab2f490eec5b46c5231fbbb699d7
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-17 02:17:52 -0700

    task 9 copying string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copies string pointed to.
+ * @dest: pointer
+ * @src: pointer
+ * Return:pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int n;
+
+	for (n = 0; src[n] != '\n'; n++)
+		dest[n] = src[n];
+	dest[n] = '\0';
+	return (dest);
+}
