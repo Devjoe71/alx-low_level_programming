e01325ebecb5b165d320e56d764fb82b9ac81a3a
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-18 13:40:57 -0700

    task 2 copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: whatever is in src is copied here
+ * @src: elements in here are copied to dest
+ * @n: elements to be coupied
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int m;
+
+	for (m = 0; m < n && src[m] != '\0'; m++)
+		dest[m] = src[m];
+	while (m < n)
+	{
+		dest[m] = '\0';
+		m++;
+	}
+	return (dest);
+}
