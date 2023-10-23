7f91305062f86faa19ad666bc871fedea8722aff
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-23 14:40:55 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk -  searches  a string for any set of byte
+ * @s: where the search is done
+ * @accept: where the comparison is done
+ * Return: return null if no comparison
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[i] == accept[j]; j++)
+			return (s);
+	}
+	return (NULL);
+}
