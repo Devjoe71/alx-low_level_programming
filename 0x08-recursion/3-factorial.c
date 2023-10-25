90691e95682e018c929493b23bd6016a4efa6b7c
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-25 11:42:26 -0700

    task 3, factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * factorial - prints factorial
+ * @n: parameter
+ * Return: -1 if n is less than zero
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		n = n * factorial(n - 1);
+	}
+	return (n);
+}
