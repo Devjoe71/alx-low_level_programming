dfcaf981a73d281d03068b0cd906f14b9441c8a5
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-28 09:40:19 -0700

    task 0, whatsmyname

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main  - prints the number of arguments passed into it
+ * @argc: parameter
+ * @argv: pointer to string
+ * Return: zero
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d |n", agrc);
+	return (0);
+}
