5bf8a55698c576f683f0872c2cc28f2bd43f4e1a
Joseph Karanja <josephkaranja7161@gmail.com>
2023-10-28 09:26:05 -0700

    task 0, whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,8 @@
  * Return: in our case this returns zero
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((unused))  argc, char *argv[])
 {
-	(void) argc;
-
 	printf("%s \n", argv[0]);
 	return (0);
 }
