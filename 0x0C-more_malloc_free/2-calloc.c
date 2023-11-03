5007bf3f13a673db5bf8f3656ccd7fa55a8b78d2
Joseph Karanja <josephkaranja7161@gmail.com>
2023-11-03 06:30:27 -0700

    task 2, use of calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: parameter - elements
+ * @size: size of an array
+ * Return: returns zero
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = calloc(nmemb, size);
+	if (ptr == NULL)
+		return (NULL);
+	else
+		return (ptr);
+}
