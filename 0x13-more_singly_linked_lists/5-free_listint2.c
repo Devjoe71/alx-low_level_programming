8130ea3d9e0c028c360007c8c85b3da7c7ebe915
Joseph Karanja <josephkaranja7161@gmail.com>
2023-11-20 23:40:14 -0800

    task 5, freeing a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - frees a list
+ * @head: points to the address of the first element in the list
+ *
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tempo;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		tempo = (*head)->next;
+		free(*head);
+		*head = tempo;
+	}
+	head = NULL;
+}
