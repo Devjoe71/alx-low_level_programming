34b456b9d27ae355424487144a5c287035400a2a
Joseph Karanja <josephkaranja7161@gmail.com>
2023-11-20 22:28:48 -0800

    task 0, func that prints all elements of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t newnode = 0;
+	size_t elements = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		newnode++;
+		elements++;
 	}
-	return (newnode);
+	return (elements);
 }
