364df645c3e25338cb923b45876291e8bb8afda8
Joseph Karanja <josephkaranja7161@gmail.com>
2023-12-01 04:10:00 -0800

    task 1 modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,22 +10,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fl;
+	int fn;
 	int number_letters;
 	int rewrite;
 
 	if (!filename)
 		return (-1);
-	fl = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fl == -1)
+	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fn == -1)
 		return (-1);
 	if (!text_content)
 		text_content = "";
 	for (number_letters = 0; text_content[number_letters]; number_letters++)
 		;
-	rewrite = write(fl, text_content, number_letters);
+	rewrite = write(fn, text_content, number_letters);
 	if (rewrite == -1)
 		return (-1);
-	close(fl);
+	close(fn);
 	return (1);
 }
